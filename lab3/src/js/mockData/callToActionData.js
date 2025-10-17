de35a2d4f48b21eec371fc4a96317e5c883ec4fc
MMMMMMar22 <klever.vanlav@gmail.com>
2025-10-17 13:46:34 +0500

    Delete lab3 directory

--- lab3/src/js/mockData/callToActionData.js
+++
@@ -1,9 +0,0 @@
-const callToActionData = {
-  topCta: "Запросить ранний доступ",
-  header: "Зарегистрируйтесь и начните исследовать безграничные возможности.",
-  buttonText: "Начать",
-};
-
-export default callToActionData;
-
-
